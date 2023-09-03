aed70269db56cdb072e6ddb454eb9cdfdb9dcf9f
yuvaraj-joel <109791602+yuvaraj-joel@users.noreply.github.com>
2023-09-03 12:29:43 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const express =require("express");
+const express=require("express");
+const { trains } =require("../Tranin Management/Data/train.json");
 
-const isPrime = require("prime-number")
-const {numbers} = require("../124015128/Data/data.json")
-// const userRouter=require("./routes/number.js");
 const app=express();
 const port=8080;
 app.use(express.json());
+
+
 // check for server
 app.get("/",(req,res)=>{
     res.send("Server is Up and Working now!")
@@ -15,25 +15,148 @@ app.get("/",(req,res)=>{
 });
 
 
-app.get("/prime",(req,res)=>{
-    const {data}=req.body;
-    
 
-    const prime=data.map((each)=>{
-        if(isPrime(each)){
+// Route:/train
+// Method:GET
+// Description:Get train
+// Access:public
+// Paramenter:none
+
+app.get('/trains',(req,res)=>{
+    res.status(200).json({
+        success:true,
+        data:trains
+    });
+});
+
+
+/*
+Route:/trains
+Method:GET
+Description:Get the user
+Access:public
+Paramenter:id
+*/
+
+
+
+
+app.get("/trains/:id",(req,res)=>{
+    const {id}=req.params;
+    // console.log(id);
+    const book = trains.find((each)=>each.id=== id);
+
+    if (!book){
+        res.status(404).json({
+            success:false,
+            message:"Name Not Found",
+        });  
+    }else {
+      res.status(200).json({
+            success:true,
+            data:train,
+        });
+
+    }
+});
+
+
+
+
+/*
+Route:/trains
+Method:POST
+Description:Create a new user
+Access:public
+Paramenter:none
+*/
+
+
+
+app.post("/trains",(req,res)=>{
+    const {id,name,country,language,trainName,Price,Departure,Tickets}=req.body;
+    const train=trains.find((each)=>each.id===id);
+    if(train){
+       return res.status(404).json({
+            sucess:false,
+            message:"Name Alread Exists"
+        });
+    }
+        trains.push({
+            id,
+            name,
+            country,
+            language,
+            trainName,
+            Price,
+            Departure,
+            Tickets
+        });
+
+       return res.status(201).json({
+            success:true,
+            data:trains,
+        });
+    }
+);
+
+/*
+Route:/trains/:id
+Method:PUT
+Description:Update a new name
+Access:public
+Paramenter:id
+*/
+app.put("/trains/:id",(req,res)=>{
+    const {id}=req.params;
+    const {data}=req.body;
+    const train=trains.find((each)=>each.id===id)
+    if(!book){
+        return res.status(404).json({
+            success:true,
+            message:"name not Found"
+        });
+    }
+
+    const updateUser=train.map((each)=>{
+        if(each.id===id){
             return{
                 ...each,
+                ...data,
             };
         }
         return each;
     });
     return res.status(200).json({
         success:true,
-        data:prime,
+        data:updateUser,
     });
 });
+/*
+Route:/books/:id
+Method:DELETE
+Description:Deleting User By id
+Access:public
+Paramenter:id
+*/
 
+app.delete("/:id",(req,res)=>{
+    const {id}=req.params;
+    const train=trains.find((each)=>each.id=== id);
+    if(!train){
+        return res.status(404).json({
+            success:false,
+            message:"Name Not Found",
+        })
+    }
 
+    const index=trains.indexOf(train);
+    books.splice(index,1)
+    return res.status(200).json({
+        success:true,
+        data:trains,
+    })
+})
 
 app.get("*",(req,res)=>{
     res.send("Oopss...There no Route now!")
